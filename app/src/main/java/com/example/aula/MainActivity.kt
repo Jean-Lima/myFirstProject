3c1f1ea08554a9f6543a5501f2b020c9dde0b264
Julio Cesar da Costa Reis Filho <juliofilho@brq.com>
2020-05-22 00:16:55 -0300

    first commit

---
+++ app/src/main/java/com/example/aula/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.aula
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+
+    fun init() {
+        experimente.setOnClickListener {
+            val novaTela = Intent(this, HomeActivity::class.java)
+            startActivity(novaTela)
+        }
+    }
+}
+
