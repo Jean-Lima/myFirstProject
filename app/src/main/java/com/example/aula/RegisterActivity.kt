96c07dbea05e0ddfc9483eab9d871c560261f621
Jean Lima <ge.lima@hotmail.com.br>
2020-05-27 00:17:42 -0300

    validação do usuário

--- app/src/main/java/com/example/aula/RegisterActivity.kt
+++ app/src/main/java/com/example/aula/RegisterActivity.kt
@@ -1,7 +1,11 @@
 package com.example.aula
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.include_toolbar.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -13,5 +17,86 @@ class RegisterActivity : AppCompatActivity() {
         supportActionBar?.title = "CADASTRO"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setBackgroundDrawable(getDrawable(R.drawable.background_toolbar))
+
+        initButton()
+    }
+
+    private fun initButton() {
+        registerButton.setOnClickListener {
+            ValidateFields()
+        }
+    }
+
+    private fun ValidateFields() {
+
+        var fullName: String? = fullNameInput.text.toString()
+        var gender: String? = if (!maleRadioButton.isChecked) "Masculino" else "Feminino"
+        var bday: String? = dateInput.text.toString()
+        var user: String? = userInput.text.toString()
+        var password: String? = passwordInput.text.toString()
+        var confirmPassword: String? = confirmPasswordInput.text.toString()
+        var email: String? = emailInput.text.toString()
+        var cpf: String? = cpfInput.text.toString()
+        var language: String? = null
+
+        if (password != confirmPassword) {
+            return errorDialog("As senhas devem combinar")
+        }
+
+        if (password.isNullOrEmpty()) {
+            return errorDialog("Senha obrigatória")
+        }
+
+        if (fullName.isNullOrEmpty()) {
+            return errorDialog("Nome obrigatório")
+        }
+
+        if (!maleRadioButton.isChecked && !femaleRadioButton.isChecked) {
+            return errorDialog("Gênero obrigatório")
+        }
+
+        if (bday.isNullOrEmpty()) {
+            return errorDialog("Data obrigatória")
+        }
+
+        if (user.isNullOrEmpty()) {
+            return errorDialog("Usuário obrigatório")
+        }
+
+        if (email.isNullOrEmpty()) {
+            return errorDialog("Email obrigatório")
+        }
+
+        if (cpf.isNullOrEmpty()) {
+            return errorDialog("CPF obrigatório")
+        }
+
+        createUser(fullName, gender!!, bday, user, password, email, cpf, language)
+
+    }
+
+    private fun createUser(
+        fullName: String,
+        gender: String,
+        bday: String,
+        user: String,
+        password: String,
+        email: String,
+        cpf: String,
+        language: String?
+    ) {
+        UserModel(fullName, gender, bday, user, password, email, cpf, language)
+
+        errorDialog("$fullName \n$gender \n$bday \n$user \n$password \n$email \n$cpf \n$language")
+    }
+
+
+    private fun errorDialog(msg: String) {
+        AlertDialog.Builder(this)
+            .setTitle("Erro")
+            .setMessage(msg)
+            .setNegativeButton("OK", null)
+            .create()
+            .show()
     }
 }
---
+++ app/src/main/java/com/example/aula/UserModel.kt
@@ -0,0 +1,14 @@
+package com.example.aula
+
+class UserModel (
+
+    var fullName: String? = null,
+    var gender: String? = null,
+    var bday: String? = null,
+    var user: String? = null,
+    var password: String? = null,
+    var email: String? = null,
+    var cpf: String? = null,
+    var language: String? = null
+
+)
