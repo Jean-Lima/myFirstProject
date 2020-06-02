7b103153d4c3d151044f0672d815c85ec1030e8b
Julio Cesar da Costa Reis Filho <juliofilho@brq.com>
2020-06-02 00:00:50 -0300

    foi incrementado uma requisição da internet

--- app/src/main/java/com/example/aula/HomeActivity.kt
+++ app/src/main/java/com/example/aula/HomeActivity.kt
@@ -2,7 +2,11 @@ package com.example.aula
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
 import kotlinx.android.synthetic.main.include_toolbar.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeActivity : AppCompatActivity() {
 
@@ -12,5 +16,26 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.title = "BEM VINDO"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        request()
+
     }
+
+    fun request() {
+        Api().retrofit.create(ApiInterface::class.java).getList()
+            .enqueue(object : Callback<List<PostModel>> {
+                override fun onFailure(call: Call<List<PostModel>>, t: Throwable) {
+                    d("testando", "falhou")
+                }
+
+                override fun onResponse(
+                    call: Call<List<PostModel>>,
+                    response: Response<List<PostModel>>
+                ) {
+                    var lista = response.body()
+                    var test = lista
+                }
+
+            })
+    }
+
 }
