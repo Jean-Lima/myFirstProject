797f5548043e8394679d56a6c49a84af1ca0fb8d
Jean Lima <ge.lima@hotmail.com.br>
2020-06-02 23:20:49 -0300

    minhaLista

--- app/src/main/java/com/example/aula/HomeActivity.kt
+++ app/src/main/java/com/example/aula/HomeActivity.kt
@@ -1,7 +1,10 @@
 package com.example.aula
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.include_toolbar.*
 
 class HomeActivity : AppCompatActivity() {
@@ -9,8 +12,25 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        initToolbar()
+
+        initButtom()
+    }
+
+    private fun initButtom() {
+        cardView.setOnClickListener {
+            val novaTela = Intent(this, SendActivity::class.java)
+            startActivity(novaTela)
+        }
+    }
+
+    private fun initToolbar() {
         setSupportActionBar(toolbar)
         supportActionBar?.title = "BEM VINDO"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+
+
 }
